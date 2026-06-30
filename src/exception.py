a9e88a3a7ede0826d7dfc20f898ea024e9156737
Pranjal Gupta <Pranjal.iitd27@gmail.com>
2026-06-30 14:54:00 +0530

    EDA adn Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,7 @@
 import sys
-import logging
+# import logging
+from src.logger import logging 
+# this will put the error in the log file instead of printing it in the console.
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -15,9 +17,9 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-# if __name__ == "__main__":
-#     try:
-#         a = 1 / 0
-#     except Exception as e:
-#         logging.info("Divide by zero error occurred.")
-#         raise CustomException(e, sys)
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("Divide by zero error occurred.")
+        raise CustomException(e, sys)
